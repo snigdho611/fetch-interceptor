1c4d1dde7a8f468e77fa7d504a1e29fad48d7124
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2022-09-15 21:13:15 +0600

    Resolved a bug

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     if (intercept) {
-      console.log(intercept)
+      console.log(intercept);
       interceptor();
     }
   }, [intercept]);
@@ -24,10 +24,9 @@ function App() {
       });
   };
 
-
   return (
     <div className="main">
-      <div>
+      <div className="main__header">
         <button
           onClick={() => {
             onClickPosts();
@@ -35,43 +34,49 @@ function App() {
         >
           Posts
         </button>
-        <button value="intercept" onClick={(e) => { setIntercept(!intercept); }}>Intercept</button>
+        <button
+          value="intercept"
+          onClick={(e) => {
+            setIntercept(true);
+          }}
+        >
+          Intercept
+        </button>
       </div>
-      <div>{intercept ? "Posts will now be rerouted to quotes" : null}</div>
+      <div className="main__header">
+        {intercept ? "Posts will now be rerouted to quotes" : null}
+      </div>
       <div className="main__container">
         <div className="main__container__content">
           <div className="main__container__content__cell">
             {data && data.posts ? null : "Posts will appear here, none to show now"}
-            {data && data.posts && data.posts.map((element, i) => {
-              return (
-                <div
-                  key={i}
-
-                >
-                  <span style={{ fontWeight: "bold", fontSize: "17px" }}>{element.title}</span>
-                  <span>{element.body}</span>
-                  <span>Tags: {element.tags && element.tags.join()}</span>
-                </div>
-              );
-            })}</div>
+            {data &&
+              data.posts &&
+              data.posts.map((element, i) => {
+                return (
+                  <div key={i} className="main__container__content__cell__row">
+                    <span style={{ fontWeight: "bold", fontSize: "17px" }}>{element.title}</span>
+                    <span>{element.body}</span>
+                    <span>Tags: {element.tags && element.tags.join()}</span>
+                  </div>
+                );
+              })}
+          </div>
           <div className="main__container__content__cell">
             {data && data.quotes ? null : "Quotes will appear here, none to show now"}
-            {data && data.quotes && data.quotes.map((element, i) => {
-              return (
-                <div
-                  key={i}
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: "0.25rem",
-                    margin: "1.25rem 0.75rem",
-                  }}
-                >
-                  <span style={{ fontWeight: "bold", fontSize: "17px" }}>By: {element.author}</span>
-                  <span style={{ fontStyle: "italic" }}>"{element.quote}"</span>
-                </div>
-              );
-            })}</div>
+            {data &&
+              data.quotes &&
+              data.quotes.map((element, i) => {
+                return (
+                  <div key={i} className="main__container__content__cell__row">
+                    <span style={{ fontWeight: "bold", fontSize: "17px" }}>
+                      By: {element.author}
+                    </span>
+                    <span style={{ fontStyle: "italic" }}>"{element.quote}"</span>
+                  </div>
+                );
+              })}
+          </div>
         </div>
       </div>
     </div>
