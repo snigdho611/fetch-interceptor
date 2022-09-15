c0de45803fc34508807dbd105d94797a6a22089e
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2022-09-15 16:42:28 +0600

    Update intercept.js

--- src/intercept.js
+++ src/intercept.js
@@ -3,16 +3,10 @@ export const interceptor = async () => {
   window.fetch = async (...args) => {
     let [resource, config] = args;
     const URLcutOff = "https://dummyjson.com";
-    // const leading = resource.slice(0, URLcutOff.length);
     const trailing = resource.slice(URLcutOff.length);
     console.log(trailing);
     if (trailing.startsWith("/posts")) {
-      // console.log("Quotes");
-      resource = `https://dummyjson.com/carts?limit=1`;
-    }
-    if (trailing.startsWith("/quotes")) {
-      // console.log("Posts");
-      resource = `https://dummyjson.com/todos?limit=5`;
+      resource = `https://dummyjson.com/quotes?limit=5`;
     }
     const response = await originalFetch(resource, config);
     return response;
